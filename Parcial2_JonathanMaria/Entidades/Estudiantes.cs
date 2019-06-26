6349744896a36f28eb197d731a502b5ae0442b23
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-26 00:04:47 -0400

    Agregando las llaves primarias a las Entidades

--- Parcial2_JonathanMaria/Entidades/Estudiantes.cs
+++ Parcial2_JonathanMaria/Entidades/Estudiantes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Parcial2_JonathanMaria.Entidades
 {
     public class Estudiantes
     {
+        [Key]
         public int EstudianteId { get; set; }
         public DateTime FechaIngreso { get; set; }
         public string Nombre { get; set; }
---
+++ Parcial2_JonathanMariaTests/Entidades/EstudiantesTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_JonathanMaria.BLL;
+using Parcial2_JonathanMaria.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JonathanMaria.Entidades.Tests
+{
+    [TestClass()]
+    public class EstudiantesTests
+    {
+        [TestMethod()]
+        public void Guardar()
+        {
+            RepositorioBase<Estudiantes> repositorio  = new RepositorioBase<Estudiantes>();
+            Estudiantes estudiante = new Estudiantes();
+            estudiante.EstudianteId = 1;
+            estudiante.FechaIngreso = DateTime.Now;
+            estudiante.Nombre = "Jonathan Maria Parra";
+            estudiante.Balance = 0;
+            Assert.IsTrue(repositorio.Guardar(estudiante));
+        }
+    }
+}
