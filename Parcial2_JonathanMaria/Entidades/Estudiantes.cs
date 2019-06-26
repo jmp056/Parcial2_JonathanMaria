49869b0d96d3fdd7923a0b9caec8f6d3227a2cad
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-25 20:33:15 -0400

    Terminando la entidad Estudiantes

---
+++ Parcial2_JonathanMaria/Entidades/Estudiantes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JonathanMaria.Entidades
+{
+    public class Estudiantes
+    {
+        public int EstudianteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombre { get; set; }
+        public decimal Balance { get; set; }
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngreso = DateTime.Now;
+            Nombre = string.Empty;
+            Balance = 0;
+        }
+    }
+}
