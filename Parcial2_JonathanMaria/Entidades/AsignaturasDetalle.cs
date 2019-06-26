a1677737c9d1fd8ee1342a0ce445bb26ca3884c4
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-25 20:43:31 -0400

    Terminando la entidad Asignaturas Detalle

---
+++ Parcial2_JonathanMaria/Entidades/AsignaturasDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JonathanMaria.Entidades
+{
+    public class AsignaturasDetalle
+    {
+        public int AsignaturaDetalleId { get; set; }
+        public int InscripcionId { get; set; }
+        public int EstudianteId { get; set; }
+
+        [ForeignKey("EstudianteId")]
+        public virtual Estudiantes Estudiante { get; set; }
+        public decimal Balance { get; set; }
+
+        public AsignaturasDetalle(int estudianteId, Estudiantes estudiante, decimal balance)
+        {
+            EstudianteId = estudianteId;
+            Estudiante = estudiante;
+            Balance = balance;
+        }
+    }
+}
