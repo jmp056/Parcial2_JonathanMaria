0a36775730f66b5d8b7794a95d2ef8f37c40a7c1
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-25 20:18:59 -0400

    Terminando la entidad Asignaturas

---
+++ Parcial2_JonathanMaria/Entidades/Asignaturas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JonathanMaria.Entidades
+{
+    public class Asignaturas
+    {
+        public int AsignaturaId { get; set; }
+        public string Descripcion { get; set; }
+        public int Creditos { get; set; }
+        public Asignaturas()
+        {
+            AsignaturaId = 0;
+            Descripcion = string.Empty;
+            Creditos = 0;
+        }
+
+    }
+}
