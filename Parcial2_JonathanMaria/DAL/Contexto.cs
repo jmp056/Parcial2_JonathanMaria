5ad8e2c25da6ded5f73f331e689b8e0c45e9100f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-25 21:01:08 -0400

    Terminando el contexto

---
+++ Parcial2_JonathanMaria/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Parcial2_JonathanMaria.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JonathanMaria.DAL
+{
+    public class Contexto
+    {
+        public DbSet<Asignaturas> Asignaturas { get; set; }
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+        public DbSet<Inscripciones> Inscripciones { get; set; }
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
