dc5cffe5b17cce2c5c97c15ca76dc103046354a6
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-26 00:09:13 -0400

    Terminando el Test a la entidad Estudiantes

--- Parcial2_JonathanMariaTests/Entidades/EstudiantesTests.cs
+++ Parcial2_JonathanMariaTests/Entidades/EstudiantesTests.cs
@@ -17,11 +17,45 @@ namespace Parcial2_JonathanMaria.Entidades.Tests
         {
             RepositorioBase<Estudiantes> repositorio  = new RepositorioBase<Estudiantes>();
             Estudiantes estudiante = new Estudiantes();
-            estudiante.EstudianteId = 1;
+            estudiante.EstudianteId = 2;
             estudiante.FechaIngreso = DateTime.Now;
             estudiante.Nombre = "Jonathan Maria Parra";
             estudiante.Balance = 0;
             Assert.IsTrue(repositorio.Guardar(estudiante));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            Estudiantes estudiante = new Estudiantes();
+            estudiante.EstudianteId = 1;
+            estudiante.FechaIngreso = DateTime.Now;
+            estudiante.Nombre = "Jonathan Maria";
+            estudiante.Balance = 0;
+            Assert.IsTrue(repositorio.Modificar(estudiante));
+        }
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            List<Estudiantes> lista = new List<Estudiantes>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
         }
     }
 }
