53828e278341e9f11149927ba468526d4b6ca34e
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-27 00:33:29 -0400

    Programando algunos metodos de el registro de asignaturas

---
+++ Parcial2_JonathanMaria/UI/Registros/rAsignaturas.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Parcial2_JonathanMaria.UI.Registros
+{
+    partial class rAsignaturas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.AsignaturaIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DescripcionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.CreditosNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.AsignaturaIdNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CreditosNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NuevoButton.Location = new System.Drawing.Point(32, 149);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(75, 23);
+            this.NuevoButton.TabIndex = 23;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuardarButton.Location = new System.Drawing.Point(165, 149);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(75, 23);
+            this.GuardarButton.TabIndex = 22;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            this.GuardarButton.Click += new System.EventHandler(this.GuardarButton_Click);
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EliminarButton.Location = new System.Drawing.Point(331, 149);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 23);
+            this.EliminarButton.TabIndex = 21;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.Location = new System.Drawing.Point(29, 69);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(91, 16);
+            this.Nombre.TabIndex = 19;
+            this.Nombre.Text = "Descripcion";
+            // 
+            // AsignaturaIdNumericUpDown
+            // 
+            this.AsignaturaIdNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AsignaturaIdNumericUpDown.Location = new System.Drawing.Point(178, 36);
+            this.AsignaturaIdNumericUpDown.Name = "AsignaturaIdNumericUpDown";
+            this.AsignaturaIdNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.AsignaturaIdNumericUpDown.TabIndex = 17;
+            // 
+            // DescripcionTextBox
+            // 
+            this.DescripcionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescripcionTextBox.Location = new System.Drawing.Point(185, 66);
+            this.DescripcionTextBox.Name = "DescripcionTextBox";
+            this.DescripcionTextBox.Size = new System.Drawing.Size(120, 22);
+            this.DescripcionTextBox.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Materia Id";
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuscarButton.Location = new System.Drawing.Point(335, 38);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 13;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // CreditosNumericUpDown
+            // 
+            this.CreditosNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreditosNumericUpDown.Location = new System.Drawing.Point(185, 94);
+            this.CreditosNumericUpDown.Name = "CreditosNumericUpDown";
+            this.CreditosNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.CreditosNumericUpDown.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Creditos";
+            // 
+            // rAsignaturas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CreditosNumericUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.AsignaturaIdNumericUpDown);
+            this.Controls.Add(this.DescripcionTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BuscarButton);
+            this.Name = "rAsignaturas";
+            this.Text = "rAsignaturas";
+            ((System.ComponentModel.ISupportInitialize)(this.AsignaturaIdNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CreditosNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.NumericUpDown AsignaturaIdNumericUpDown;
+        private System.Windows.Forms.TextBox DescripcionTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.NumericUpDown CreditosNumericUpDown;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Parcial2_JonathanMaria/UI/Registros/rAsignaturas.cs
@@ -0,0 +1,83 @@
+﻿using Parcial2_JonathanMaria.BLL;
+using Parcial2_JonathanMaria.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial2_JonathanMaria.UI.Registros
+{
+    public partial class rAsignaturas : Form
+    {
+        public rAsignaturas()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            AsignaturaIdNumericUpDown.Value = 0;
+            DescripcionTextBox.Text = string.Empty;
+            CreditosNumericUpDown.Value = 0;
+        }
+
+        private Asignaturas LlenaClase()
+        {
+            Asignaturas Asignatura = new Asignaturas();
+            Asignatura.AsignaturaId = Convert.ToInt32(AsignaturaIdNumericUpDown.Value);
+            Asignatura.Descripcion = DescripcionTextBox.Text;
+            Asignatura.Creditos = Convert.ToInt32(CreditosNumericUpDown.Value);
+            return Asignatura;
+        }
+
+        private void LlenaCampos(Asignaturas Asignatura)
+        {
+            AsignaturaIdNumericUpDown.Value = Asignatura.AsignaturaId;
+            DescripcionTextBox.Text = Asignatura.Descripcion;
+            CreditosNumericUpDown.Value = Asignatura.Creditos;
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            RepositorioBase<Asignaturas> repositorio = new RepositorioBase<Asignaturas>();
+            Asignaturas Asignatura = repositorio.Buscar((int)AsignaturaIdNumericUpDown.Value);
+            return Asignatura != null;
+        }
+
+        private void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Asignaturas> repositorio = new RepositorioBase<Asignaturas>();
+            Asignaturas Asignatura;
+            Asignatura = LlenaClase();
+            repositorio.Guardar(Asignatura);
+        }
+
+        private void EliminarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Asignaturas> repositorio = new RepositorioBase<Asignaturas>();
+            int id;
+            int.TryParse(AsignaturaIdNumericUpDown.Text, out id);
+            repositorio.Eliminar(id);
+        }
+
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Asignaturas> repositorio = new RepositorioBase<Asignaturas>();
+            int id;
+            Asignaturas Asignatura = new Asignaturas();
+            int.TryParse(AsignaturaIdNumericUpDown.Text, out id);
+            Asignatura = repositorio.Buscar(id);
+            LlenaCampos(Asignatura);
+        }
+    }
+}
