99f02acb4687adc44821e9bd12885a3df75d3eb4
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-27 00:19:20 -0400

    Agregando el registro de estudiantes al formulario principal

--- Parcial2_JonathanMaria/MainForm.cs
+++ Parcial2_JonathanMaria/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Parcial2_JonathanMaria.UI.Registros;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace Parcial2_JonathanMaria
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void RegistrarEstudianteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rEstudiantes rE = new rEstudiantes();
+            rE.ShowDialog();
         }
     }
 }
